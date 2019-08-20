79222a5c231672b740b4026eb3e5505ebcba5dcf
朱文赵 <1392936858@qq.com>
2019-08-21 00:07:06 +0800

    flink的join 两个操作

--- src/main/java/org/ximo/finkinaction/java/dataset/DataSetJoinApp.java
+++ src/main/java/org/ximo/finkinaction/java/dataset/DataSetJoinApp.java
@@ -3,8 +3,12 @@ package org.ximo.finkinaction.java.dataset;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.operators.DataSource;
+import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.util.Collector;
+import scala.Int;
 
 import java.util.List;
 import java.util.stream.Stream;
@@ -40,6 +44,23 @@ public class DataSetJoinApp {
         userInput.join(storeInput)
                 .where("zip")
                 .equalTo("zip")
+                // with 可以指定返回列 join的实现
+                .with((user, store) -> new Tuple3<>(user.zip, user.name, store.mgr.mName))
+                // lambda要指定类型
+                .returns(Types.TUPLE(Types.INT, Types.STRING, Types.STRING))
+                .print();
+
+        System.out.println("分割线————————————————");
+
+        // void join (IN1 first, IN2 second, Collector<OUT> out)
+        userInput.join(storeInput)
+                .where("zip")
+                .equalTo("zip")
+                // flatJoin的实现
+                .with((User user, Store store, Collector<Tuple3<Integer, String, String>> col) -> {
+                    col.collect(new Tuple3<>(user.zip, user.name, store.mgr.mName));
+                })
+                .returns(Types.TUPLE(Types.INT, Types.STRING, Types.STRING))
                 .print();
     }
 
