4fb182d3e849b11e0565caabb9fa8e38abc6c076
朱文赵 <1392936858@qq.com>
2019-09-05 00:39:30 +0800

    添加DataStream -> SplitStream select split

--- src/main/java/org/ximo/finkinaction/java/datastream/DataStreamTransformationsApp.java
+++ src/main/java/org/ximo/finkinaction/java/datastream/DataStreamTransformationsApp.java
@@ -1,5 +1,6 @@
 package org.ximo.finkinaction.java.datastream;
 
+import org.apache.flink.streaming.api.datastream.SplitStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 import java.util.List;
@@ -17,15 +18,26 @@ public class DataStreamTransformationsApp {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         List<Integer> list = Stream.iterate(0, i -> i + 1).limit(100).collect(toList());
 
-        env.fromCollection(list)
-                .map(i -> i * 2)
-                .filter(i -> i % 2 == 0)
-                .print()
-                .setParallelism(1);
+//        env.fromCollection(list)
+//                .map(i -> i * 2)
+//                .filter(i -> i % 2 == 0)
+//                .print()
+//                .setParallelism(1);
+//
+//        // union
+//        env.fromElements(1, 2).union(env.fromElements(3, 4))
+//                .print();
 
-        // union
-        env.fromElements(1, 2).union(env.fromElements(3, 4))
-                .print();
+        final SplitStream<Integer> split = env.fromCollection(list)
+                .split(num -> {
+                    if (num % 2 == 0) {
+                        return Stream.of("even").collect(toList());
+                    } else {
+                        return Stream.of("odd").collect(toList());
+                    }
+                });
+
+        split.select("odd").print().setParallelism(1);
 
         env.execute();
 
--- src/main/scala/org/ximo/finkinaction/scala/datastream/DataStreamTransformationsApp.scala
+++ src/main/scala/org/ximo/finkinaction/scala/datastream/DataStreamTransformationsApp.scala
@@ -1,7 +1,9 @@
 package org.ximo.finkinaction.scala.datastream
 
 import org.apache.flink.api.scala._
-import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.apache.flink.streaming.api.scala.{DataStream, SplitStream, StreamExecutionEnvironment}
+
+import scala.collection.immutable
 
 /**
   *
@@ -13,18 +15,29 @@ object DataStreamTransformationsApp extends App {
 
   val env = StreamExecutionEnvironment.getExecutionEnvironment
 
-  val list = 1 to 10
+  val list: immutable.Seq[Int] = 1 to 10
 
-  val data = env.fromCollection(list)
+  val data: DataStream[Int] = env.fromCollection(list)
 
-  data.map(_ * 2).filter(_ % 2 == 0).print().setParallelism(1)
+//  data.map(_ * 2).filter(_ % 2 == 0).print().setParallelism(1)
+//
+//  // union
+//  val data2 = env.fromCollection(11 to 20)
+//
+//
+//  println("----------------")
+//  data.union(data2).print().setParallelism(1)
 
-  // union
-  val data2 = env.fromCollection(11 to 20)
+  // todo side outputs
+  val split = data.split(
+    (num: Int) =>
+      (num % 2) match {
+        case 0 => List("even")
+        case 1 => List("odd")
+      }
+  )
 
-
-  println("----------------")
-  data.union(data2).print().setParallelism(1)
+  split.select("even").print().setParallelism(1)
 
   env.execute()
 
