22c1f2349efb84e7c96606ba2617810385d16988
朱文赵 <1392936858@qq.com>
2019-12-03 01:00:13 +0800

    tableApi的操作

--- src/main/java/org/ximo/finkinaction/java/tableapiandsql/TableEnvApp.java
+++ src/main/java/org/ximo/finkinaction/java/tableapiandsql/TableEnvApp.java
@@ -1,5 +1,7 @@
 package org.ximo.finkinaction.java.tableapiandsql;
 
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.operators.DataSource;
@@ -7,6 +9,7 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.java.BatchTableEnvironment;
 import org.apache.flink.table.api.java.StreamTableEnvironment;
+import org.apache.flink.table.sinks.CsvTableSink;
 import org.apache.flink.types.Row;
 import org.ximo.finkinaction.java.Person;
 import org.ximo.finkinaction.java.constants.CommonData;
@@ -38,13 +41,40 @@ public class TableEnvApp {
 
         batchTableEnvironment.toDataSet(table, Row.class).print();
 
+        // 读取一个文件操作
+        final CsvTableSink csvTableSink = new CsvTableSink("", ",");
+        String[] fieldNames = {"a", "b", "c"};
+        TypeInformation[] fieldTypes = {Types.INT, Types.STRING, Types.LONG};
+        csvTableSink.configure(fieldNames, fieldTypes);
+
+        batchTableEnvironment.registerTableSink("testTable", csvTableSink);
+
+        batchTableEnvironment.sqlUpdate(
+                "INSERT INTO RevenueFrance " +
+                        "SELECT cID, cName, SUM(revenue) AS revSum " +
+                        "FROM Orders " +
+                        "WHERE cCountry = 'FRANCE' " +
+                        "GROUP BY cID, cName"
+        );
+
+        Table inputTable = batchTableEnvironment.sqlQuery("select name, age from test1");
+        // 直接写入testTable中
+        inputTable.insertInto("testTable");
+
+        // from... 操作
+
+        // batchTableEnvironment.fromDataSet()
+        // 同样 dataStream也有类似的操作
+        // fromDataStream
+
+
         // 一些常用的操作
         StreamExecutionEnvironment streamEnv = StreamExecutionEnvironment.getExecutionEnvironment();
         StreamTableEnvironment streamTableEnv = StreamTableEnvironment.create(streamEnv);
 
-        // https://ci.apache.org/projects/flink/flink-docs-release-1.8/dev/table/streaming/dynamic_tables.html#table-to-stream-conversion
+        // https://ci.apache.org/projects/flink/flink-docs-release-1.9/zh/dev/table/common.html#convert-a-table-into-a-datastream
         // 可回收流，就是可修改的
-//        streamTableEnv.toAppendStream()
+        // DataStream<Row> dsRow = streamTableEnv.toAppendStream(table, Row.class);
     }
 
 }
