d4c38b333c6304da669d8ab709530c6450831e88
朱文赵 <zwz@servyou.com.cn>
2019-12-17 19:56:38 +0800

    flink 的 迭代函数 使用

--- src/test/java/org/ximo/flinkincation/java/dataset/DataSetTest.java
+++ src/test/java/org/ximo/flinkincation/java/dataset/DataSetTest.java
@@ -1,9 +1,12 @@
 package org.ximo.flinkincation.java.dataset;
 
+import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.operators.Order;
 import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.IterativeDataSet;
+import org.apache.flink.api.java.operators.MapOperator;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.util.Collector;
 import org.junit.Test;
@@ -48,4 +51,51 @@ public class DataSetTest {
         wordCounts.print();
     }
 
+    @Test
+    public void testIterativeDataSet() throws Exception {
+        final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+
+        // Create initial IterativeDataSet
+        IterativeDataSet<Integer> initial = env.fromElements(0).iterate(10000);
+
+
+        DataSet<Integer> iteration = initial.map(new MapFunction<Integer, Integer>() {
+            @Override
+            public Integer map(Integer i) throws Exception {
+                double x = Math.random();
+                double y = Math.random();
+
+                return i + ((x * x + y * y < 1) ? 1 : 0);
+            }
+        });
+
+        // Iteratively transform the IterativeDataSet
+        DataSet<Integer> count = initial.closeWith(iteration);
+
+        count.map(new MapFunction<Integer, Double>() {
+            @Override
+            public Double map(Integer count) throws Exception {
+                return count / (double) 10000 * 4;
+            }
+        }).print();
+
+        // 注释掉它 否则将会报错 说需要一个 sink的操作
+        // java.lang.RuntimeException: No new data sinks have been defined since the last execution. The last execution refers to the latest call to 'execute()', 'count()', 'collect()', or 'print()'.
+//        env.execute();
+    }
+
+    @Test
+    public void testIterativeDataSetSimple() throws Exception {
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+
+        // 循环 获得 10000 个 0
+        IterativeDataSet<Integer> initial = env.fromElements(0).iterate(10000);
+        MapOperator<Integer, Integer> iteration = initial.map(i -> {
+            System.out.println(i);
+            return i + 1;
+        });
+        initial.closeWith(iteration)
+                .print();
+
+    }
 }
